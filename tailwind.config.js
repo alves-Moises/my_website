23c94ea3507a5a21a1de0a77194cee031b284f14
r3c4 <vinicius.rech.dev@gmail.com>
2023-06-11 03:36:31 -0300

    wip+feat: implements banner and slider.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,40 +1,41 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: 'class',
+    darkMode: "class",
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.vue",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Arial', ...defaultTheme.fontFamily.sans],
+                sans: ["Arial", ...defaultTheme.fontFamily.sans],
             },
             screens: {
-                'xxsm': "420px",
-                'md': "768px",
-                'lg': "1024px",
-                'xlg': "1366px",
-                'xxlg': "1920px",
+                xxsm: "420px",
+                md: "768px",
+                lg: "1024px",
+                xlg: "1366px",
+                xxlg: "1920px",
             },
             colors: {
                 "custom-dark": {
-                    "900": "#131415"
+                    900: "#131415",
                 },
                 "night-coal": {
-                    "900": "#18181C",
-                    "800": "#222228"
+                    900: "#18181C",
+                    800: "#222228",
                 },
-                "custom-gradient": "linear-gradient(92.51deg, #FF9898 0.48%, #8054FF 100%)"
-            }
+                "primary": "#c20f29",
+                "primary-gradient":
+                    "linear-gradient(92.51deg, #FF9898 0.48%, #AE051F 100%)",
+            },
         },
-
     },
 
     plugins: [forms],
